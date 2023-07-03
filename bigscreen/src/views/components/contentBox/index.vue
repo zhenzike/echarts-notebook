73a0c40de39be69e71bc0fd622a3872c34a9b260
guo <835689414@qq.com>
2023-07-03 16:50:52 +0800

    大屏基本布局

---
+++ bigscreen/src/views/components/contentBox/index.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+     
+   </div>
+</template>
+<script>
+export default {
+   name: '',
+   components: {
+     
+   },
+   mixins: [],
+   props: {
+     
+   },
+   data() {
+     return {
+       
+     }
+   },
+   computed: {
+     
+   },
+   watch: {
+     
+   },
+   mounted() {
+     
+   },
+   methods: {
+     
+   }
+};
+</script>
+<style lang='' scoped>
+</style>
---
+++ bigscreen/src/views/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="contain">
+   
+    </div>
+</template>
+<script>
+import * as echarts from 'echarts';
+export default {
+    name: '',
+    components: {
+
+    },
+    mixins: [],
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted() {
+     
+    },
+    methods: {
+     
+    }
+};
+</script>
+<style lang='scss' scoped>
+
+.ecahrtsBox{
+    width:600px;
+    height: 400px;
+}
+</style>
